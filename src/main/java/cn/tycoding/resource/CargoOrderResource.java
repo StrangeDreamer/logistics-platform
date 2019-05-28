fe8221fbbf0c2bcbaddbae4997a9be4b7f05f89c
wangjin <3011073317@qq.com>
2019-05-28 19:15:44 +0800

    增加rabbitmq消息代理

--- src/main/java/cn/tycoding/resource/CargoOrderResource.java
+++ src/main/java/cn/tycoding/resource/CargoOrderResource.java
@@ -1,6 +1,9 @@
 package cn.tycoding.resource;
 
-import cn.tycoding.domain.CargoOrder;
+import cn.tycoding.domain.Cargo;
+import cn.tycoding.domain.CargoOrderLite;
+import cn.tycoding.exception.CargoOrderException;
+import cn.tycoding.repository.CargoRepository;
 import cn.tycoding.websocket.WebSocketServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,28 +22,37 @@ public class CargoOrderResource {
     private final Logger logger=LoggerFactory.getLogger(CargoOrderResource.class);
 
     //设置秒杀redis缓存的key
-    private final String key = "cargoOrders";
+    private final String key = "CargoOrders";
 
     @Autowired
     private RedisTemplate redisTemplate;
+    @Autowired
+    private CargoRepository cargoRepository;
 
     @PostMapping
-    public Map<String, Object> chaseCargo(@RequestBody CargoOrder cargoOrder) {
+    public Map<String, Object> chaseCargo(@RequestBody CargoOrderLite cargoOrderLite) {
         Map<String, Object> result = new HashMap<String, Object>();
-        try {
+        //获取系统时间
+        Date nowTime = new Date();
+        Cargo cargo=cargoRepository.findById(cargoOrderLite.getCargoId()).get();
 
+        if (nowTime.getTime()>cargo.getStartTime().getTime()){
+            logger.info("错过抢单时间");
+            throw new CargoOrderException("错过抢单时间");
+        }
+        try {
             logger.info("请求成功加入Redis缓存");
-            CargoOrder redisCargo = (CargoOrder) redisTemplate.boundHashOps(key).get(cargoOrder.getCargoId());
+            CargoOrderLite redisCargo = (CargoOrderLite) redisTemplate.boundHashOps(key).get(cargoOrderLite.getCargoId());
             if (redisCargo==null){
                 //存入redis缓存中(1个)。 key:秒杀表的ID值； value:秒杀表数据
-                redisTemplate.boundHashOps(key).put(cargoOrder.getCargoId(), cargoOrder);
-                WebSocketServer.sendInfo("有人出价"+cargoOrder.getCostPrice());
+                redisTemplate.boundHashOps(key).put(cargoOrderLite.getCargoId(), cargoOrderLite);
+                WebSocketServer.sendInfo("有人出价"+cargoOrderLite.getCostPrice());
             }
             else {
                 //redisCargo更大,则需要更新
-                if (redisCargo.getCostPrice().compareTo(cargoOrder.getCostPrice())==1){
-                    redisTemplate.boundHashOps(key).put(cargoOrder.getCargoId(), cargoOrder);
-                    WebSocketServer.sendInfo("有人出价"+cargoOrder.getCostPrice());
+                if (redisCargo.getCostPrice().compareTo(cargoOrderLite.getCostPrice())==1){
+                    redisTemplate.boundHashOps(key).put(cargoOrderLite.getCargoId(), cargoOrderLite);
+                    WebSocketServer.sendInfo("有人出价"+cargoOrderLite.getCostPrice());
                 }
             }
 
@@ -48,6 +61,7 @@ public class CargoOrderResource {
 
             result.put("operationResult", "排队成功");
         } catch (Exception e) {
+            e.printStackTrace();
             result.put("operationResult", "排队失败");
         }
         return result;
