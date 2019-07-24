27509dd73261aeb68aef2608c99282a1b251f4d2
StrangeDreamer <3011073317@qq.com>
2019-07-24 18:57:38 +0800

    websocket

--- db/script.sql
+++ db/script.sql
@@ -1,141 +1,143 @@
 create table bank_account
 (
-  id               INT(10) auto_increment
+  count            int auto_increment
     primary key,
-  disposable_money DOUBLE(22) not null,
-  frozen_money     DOUBLE(22) not null,
-  money            DOUBLE(22) not null
+  bank_account_log text         null,
+  available_money  double       not null,
+  created_date     datetime     not null,
+  id               int          not null,
+  modify_time      datetime     not null,
+  money            double       not null,
+  type             varchar(255) null
 );
 
 create table bid
 (
-  id           INT(10) auto_increment
+  id           int auto_increment
     primary key,
-  bidding_time DATETIME(19) null,
-  biding_money DOUBLE(22)   not null,
-  cargo_id     INT(10)      not null,
-  truck_id     INT(10)      not null
+  bid_price    double   not null,
+  cargo_id     int      not null,
+  created_time datetime not null,
+  truck_id     int      not null
 );
 
 create table cargo
 (
-  id           INT(10) auto_increment
+  id             int auto_increment
     primary key,
-  departure    VARCHAR(255)  null,
-  destination  VARCHAR(255)  null,
-  freight_fare DOUBLE(22)    not null,
-  receiver_id  INT(10)       not null,
-  shipper_id   INT(10)       not null,
-  start_time   TIMESTAMP(19) not null,
-  volume       DOUBLE(22)    not null,
-  weight       DOUBLE(22)    not null
+  abnormal       bit          not null,
+  bid_end_time   datetime     null,
+  bid_price      double       not null,
+  bid_start_time datetime     null,
+  created_time   datetime     not null,
+  departure      varchar(255) null,
+  destination    varchar(255) null,
+  freight_fare   double       not null,
+  insurance      int          not null,
+  limited_time   datetime     null,
+  overtime       bit          not null,
+  position       varchar(255) null,
+  pre_cargo_id   int          null,
+  pre_fare       double       not null,
+  receiver_id    int          not null,
+  remarks        varchar(255) null,
+  shipper_id     int          not null,
+  status         int          not null,
+  truck_id       int          not null,
+  type           varchar(255) null,
+  volume         double       not null,
+  weight         double       not null
 );
 
-create table cargo_order
+create table inspection
 (
-  id            INT(10) auto_increment
+  id                int auto_increment
     primary key,
-  cargo_id      INT(10)        not null,
-  complete_time DATETIME(19)   null,
-  cost_price    DECIMAL(19, 2) null,
-  is_abnormal   BIT(1)         not null,
-  is_overtime   BIT(1)         not null,
-  order_price   DOUBLE(22)     not null,
-  register_time DATETIME(19)   null,
-          INT(10)        not null,
-  truck_id      INT(10)        not null
+  cargo_id          int      not null,
+  created_time      datetime not null,
+  inspection_result int      not null,
+  timeout_period    int      not null
 );
 
-create table cargo_order_lite
+create table insurance_account
 (
-  id         INT(10) auto_increment
+  count                 int auto_increment
     primary key,
-  cargo_id   INT(10)        not null,
-  cost_price DECIMAL(19, 2) null,
-  truck_id   INT(10)        not null
+  insurance_account_log text         null,
+  money                 double       not null,
+  available_money       double       not null,
+  created_date          datetime     not null,
+  id                    int          not null,
+  modify_time           datetime     not null,
+  type                  varchar(255) null
 );
 
-create table guarantor_account
+create table platform
 (
-  id               INT(10) auto_increment
+  id                      int auto_increment
     primary key,
-  disposable_money DOUBLE(22) not null,
-  frozen_money     DOUBLE(22) not null,
-  money            DOUBLE(22) not null
+  biding_duration         int      not null,
+  bonus_max_ratio_in_fare double   not null,
+  create_time             datetime not null,
+  exhibition_fee          int      not null,
+  lowest_bid_price_ratio  double   not null,
+  over_time_fee_ratio     double   not null,
+  platform_profit_ratio   double   not null,
+  shipper_profit_ratio    double   not null,
+  truck_profit_ratio      double   not null,
+  withdraw_fee_ratio      double   not null
 );
 
 create table receiver
 (
-  id            INT(10) auto_increment
+  id                   int auto_increment
     primary key,
-  name          VARCHAR(255) null,
-  register_time DATETIME(19) null
-);
-
-create table seckill
-(
-  seckill_id  BIGINT(19) auto_increment
-  comment '商品ID'
-    primary key,
-  title       VARCHAR(1000)                               null
-  comment '商品标题',
-  image       VARCHAR(1000)                               null
-  comment '商品图片',
-  price       DECIMAL(10, 2)                              null
-  comment '商品原价格',
-  cost_price  DECIMAL(10, 2)                              null
-  comment '商品秒杀价格',
-  stock_count BIGINT(19)                                  null
-  comment '剩余库存数量',
-  start_time  TIMESTAMP(19) default '1970-02-01 00:00:01' not null
-  comment '秒杀开始时间',
-  end_time    TIMESTAMP(19) default '1970-02-01 00:00:01' not null
-  comment '秒杀结束时间',
-  create_time TIMESTAMP(19) default CURRENT_TIMESTAMP     not null
-  comment '创建时间'
-);
-
-create index idx_create_time
-  on seckill (end_time);
-
-create index idx_end_time
-  on seckill (end_time);
-
-create index idx_start_time
-  on seckill (start_time);
-
-create table seckill_order
-(
-  seckill_id  BIGINT(19)                              not null
-  comment '秒杀商品ID',
-  money       DECIMAL(10, 2)                          null
-  comment '支付金额',
-  user_phone  BIGINT(19)                              not null
-  comment '用户手机号',
-  create_time TIMESTAMP(19) default CURRENT_TIMESTAMP not null
-  comment '创建时间',
-  state       TINYINT(3) default -1                   not null
-  comment '状态：-1无效 0成功 1已付款',
-  primary key (seckill_id, user_phone)
+  created_date         datetime     not null,
+  id_gongsitongyidaima varchar(255) null,
+  idgerenshenfenzheng  varchar(255) null,
+  modify_time          datetime     not null,
+  name                 varchar(255) null,
+  occupation           varchar(255) null,
+  tel_number           varchar(255) null
 );
 
 create table shipper
 (
-  id            INT(10) auto_increment
+  id                   int auto_increment
     primary key,
-  name          VARCHAR(255)                            null,
-  register_time TIMESTAMP(19) default CURRENT_TIMESTAMP not null
+  activated            bit          not null,
+  bank_id              varchar(255) null,
+  created_date         datetime     not null,
+  id_gongsitongyidaima varchar(255) null,
+  idgerenshenfenzheng  varchar(255) null,
+  modify_time          datetime     not null,
+  name                 varchar(255) null,
+  occupation           varchar(255) null,
+  ranking              double       not null,
+  tel_number           varchar(255) null
 );
 
 create table truck
 (
-  id               INT(10) auto_increment
+  id                   int auto_increment
     primary key,
-  available_volume DOUBLE(22)   not null,
-  available_weight DOUBLE(22)   not null,
-  name             VARCHAR(255) null,
-  register_time    DATETIME(19) null,
-  type             INT(10)      not null
+  activated            bit          not null,
+  available_volume     double       not null,
+  available_weight     double       not null,
+  bank_id              varchar(255) null,
+  created_date         datetime     not null,
+  id_gongsitongyidaima varchar(255) null,
+  id_jiashizheng       varchar(255) null,
+  id_xingshizheng      varchar(255) null,
+  idgerenshenfenzheng  varchar(255) null,
+  insurance_id         varchar(255) null,
+  modify_time          datetime     not null,
+  name                 varchar(255) null,
+  position             varchar(255) null,
+  power                int          not null,
+  ranking              double       not null,
+  tel_number           varchar(255) null,
+  type                 varchar(255) null
 );
 
 
