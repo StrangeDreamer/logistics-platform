c6528746d62f88f17e03649471ae066c1d2d4435
StrangeDreamer <3011073317@qq.com>
2019-09-11 13:38:50 +0800

    -u

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+git pull
 mvn clean package -Dmaven.test.skip=true
-docker build -t logistics:v1.0 .
-docker run --name logistics -p 8080:8080 -d -v /opt/jar/springBootDocker/logs:/log logistics:v1.0 /bin/bash
+# docker build -t logistics:v1.0 .
+# docker run --name logistics -p 8080:8080 -d -v /opt/jar/springBootDocker/logs:/log logistics:v1.0 /bin/bash
+nohup java -jar target/springboot-seckill-0.0.1-SNAPSHOT.jar >nohup.out &
 
