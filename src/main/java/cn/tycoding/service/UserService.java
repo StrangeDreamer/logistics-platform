0272e7787e6ff0b45cc6e3d1a2999e52d5c57ef8
Xieql <727092844@qq.com>
2020-05-12 15:29:52 +0800

    修改用户服务部分代码，解决同名多用户登录问题

--- src/main/java/cn/tycoding/service/UserService.java
+++ src/main/java/cn/tycoding/service/UserService.java
@@ -1,5 +1,7 @@
 package cn.tycoding.service;
 
+import cn.tycoding.domain.Receiver;
+import cn.tycoding.domain.Shipper;
 import cn.tycoding.domain.Truck;
 import cn.tycoding.domain.User;
 import cn.tycoding.exception.TruckException;
@@ -28,14 +30,48 @@ public class UserService {
 
     @Autowired
     TruckRepository truckRepository;
+    @Autowired
+    ShipperService shipperRepository;
+    @Autowired
+    ReceiverService receiverRepository;
 
-    public Map login(String name, String password, int kind) {
-
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(name, password));
+    public Object truckLogin(String name, String password, int kind) {
         User user = (User) this.userDetailsService.loadUserByUsernameAndKind(name, kind);
         String token = jwtTokenProvider.createToken(name, user.getRoles());
         Map<Object, Object> model = new HashMap<>();
         model.put("id", user.getOwnId());
+        Truck truck=truckRepository.findTruckById(user.getOwnId());
+        model.put("weight",truck.getAvailableWeight());
+        model.put("volume",truck.getAvailableVolume());
+        model.put("filed",truck.getField());
+        model.put("name", name);
+        model.put("token", token);
+        return model;
+    }
+
+    public Object shipperLogin(String name, String password, int kind) {
+        User user = (User) this.userDetailsService.loadUserByUsernameAndKind(name, kind);
+        String token = jwtTokenProvider.createToken(name, user.getRoles());
+        Map<Object, Object> model = new HashMap<>();
+        model.put("id", user.getOwnId());
+        Shipper shipper = shipperRepository.findShipperById(user.getOwnId());
+        model.put("address",shipper.getAddress());
+        model.put("occupation",shipper.getOccupation());
+        model.put("name", name);
+        model.put("token", token);
+        return model;
+    }
+
+
+
+    public Object receiverLogin(String name, String password, int kind) {
+        User user = (User) this.userDetailsService.loadUserByUsernameAndKind(name, kind);
+        String token = jwtTokenProvider.createToken(name, user.getRoles());
+        Map<Object, Object> model = new HashMap<>();
+        model.put("id", user.getOwnId());
+        Receiver receiver = receiverRepository.findReceiverById(user.getOwnId());
+        model.put("address",receiver.getAddress());
+        model.put("occupation",receiver.getOccupation());
         model.put("name", name);
         model.put("token", token);
         return model;
@@ -52,22 +88,23 @@ public class UserService {
         return model;
     }
 
-    public Object truckLogin(String name, String password, int kind) {
-        User user = (User) this.userDetailsService.loadUserByUsernameAndKind(name, kind);
-        String token = jwtTokenProvider.createToken(name, user.getRoles());
-        Map<Object, Object> model = new HashMap<>();
-        model.put("id", user.getOwnId());
-        Truck truck=truckRepository.findTruckById(user.getOwnId());
-        model.put("weight",truck.getAvailableWeight());
-        model.put("volume",truck.getAvailableVolume());
-        model.put("filed",truck.getField());
-        model.put("name", name);
-        model.put("token", token);
-        return model;
-    }
+
 
     public String getCurrentPwd(String name,int kind){
         User user= (User) this.userDetailsService.loadUserByUsernameAndKind(name,kind);
         return user.getPassword();
+    }
+
+
+    public Map login(String name, String password, int kind) {
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(name, password));
+        User user = (User) this.userDetailsService.loadUserByUsernameAndKind(name, kind);
+        String token = jwtTokenProvider.createToken(name, user.getRoles());
+        Map<Object, Object> model = new HashMap<>();
+        model.put("id", user.getOwnId());
+        model.put("name", name);
+        model.put("token", token);
+        return model;
     }
 }
